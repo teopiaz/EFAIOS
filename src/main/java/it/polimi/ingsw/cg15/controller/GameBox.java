8bc00ed4f94f74602b5afde85495706f678aa730
Luke <Luke@192.168.2.106>
2015-06-11 11:25:35 +0200

    Fix Field Controller and Game Box classes.

--- src/main/java/it/polimi/ingsw/cg15/controller/GameBox.java
+++ src/main/java/it/polimi/ingsw/cg15/controller/GameBox.java
@@ -2,49 +2,70 @@ package it.polimi.ingsw.cg15.controller;
 
 import it.polimi.ingsw.cg15.model.GameState;
 import it.polimi.ingsw.cg15.model.player.Player;
-import it.polimi.ingsw.cg15.networking.Event;
 
 import java.util.Map;
-import java.util.concurrent.BlockingQueue;
 
+/**
+ * @author MMP - LMR
+ * The Game Box, a container which It contains the information required to the game controller.
+ */
 public class GameBox {
 
-
+    /** 
+     * A method that transforms to a string the game box information.
+     */
     @Override
     public String toString() {
-        return "GameBox [gameState=" + gameState + ", gameToken=" + gameToken + ", queue=" + queue
-                + ", players=" + players + "]";
+        return "GameBox [gameState=" + gameState + ", gameToken=" + gameToken + ", players=" + players + "]";
     }
 
-
+    /**
+     * The Game State.
+     */
     private GameState gameState;
+    
+    /**
+     * A token for the match.
+     */
     private String gameToken;
-    private BlockingQueue<Event> queue;
+    
+    /**
+     * A map with the players.
+     */
     private Map<String,Player> players;
 
-
+    /**
+     * The constructor.
+     * @param gs The Game State.
+     * @param token The Token for the match.
+     * @param players The references for the players.
+     */
     public GameBox(GameState gs,  String token, Map<String,Player> players) {
         this.gameState=gs;
         this.gameToken=token;
         this.players=players;
     }
 
-
+    /**
+     * A method that return the players.
+     * @return the players 
+     */
     public Map<String, Player> getPlayers() {
         return players;
     }
 
-
-
+    /**
+     * @return The Game State.
+     */
     public GameState getGameState() {
         return gameState;
     }
 
-
+    /**
+     * @return The Game Token.
+     */
     public String getGameToken() {
         return gameToken;
     }
-
-
 
 }
