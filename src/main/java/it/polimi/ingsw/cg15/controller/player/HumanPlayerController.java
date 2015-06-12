e576c5667c7be703ec04ada9144a87dae9dfc8f6
Luke <Luke@10.169.160.13>
2015-06-12 14:30:23 +0200

    Documentation for the Human Player Controller

--- src/main/java/it/polimi/ingsw/cg15/controller/player/HumanPlayerController.java
+++ src/main/java/it/polimi/ingsw/cg15/controller/player/HumanPlayerController.java
@@ -9,23 +9,29 @@ import it.polimi.ingsw.cg15.model.field.Coordinate;
 import it.polimi.ingsw.cg15.model.field.Field;
 
 /**
- * @author LMR - MMP
+ * @author MMP - LMR 
+ * The controller of the Human player.
  */
 public class HumanPlayerController extends PlayerController {
 
     /**
-     * 
+     *  The state of the game.
      */
     private GameState gameState;
 
-
+    /**
+     * The constructor.
+     * @param gameState
+     */
     public HumanPlayerController(GameState gameState) {
         super(gameState);
         this.gameState = gameState;
-
     }
 
-
+    /**
+     * A method that verify if a move is possible in that particular sector based on the type of the player.
+     * @param coord Coordinate where you would verify if the move is possible.
+     */
     @Override
     public boolean moveIsPossible(Coordinate coord) {
         Logger.getLogger(Field.class.getName()).log(Level.INFO,"Human move is possible");
@@ -45,13 +51,12 @@ public class HumanPlayerController extends PlayerController {
         return field.isReachable(currentPosition, destination, distance);
     }
     
+    /**
+     * A method that set the adrenaline state for the human player. In this state he can move of two cells in 1 turn.
+     */
     @Override
     public void setOnAdrenaline(){
         gameState.getTurnState().setUnderAdrenaline();
     }
     
-
-
-
-
 }
