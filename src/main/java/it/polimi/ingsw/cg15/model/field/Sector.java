c6e46111df2e1001411140f9e4550d531f7a9067
Luke <Luke@192.168.2.123>
2015-06-15 11:36:28 +0200

    Fix Sector.

--- src/main/java/it/polimi/ingsw/cg15/model/field/Sector.java
+++ src/main/java/it/polimi/ingsw/cg15/model/field/Sector.java
@@ -4,17 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The class that contains the enumeration of possible types of sector inside the map.
  * @author MMP - LMR
+ * The class that contains the enumeration of possible types of sector inside the map.
  */
 public enum Sector {
     GREY(1), WHITE(2), HATCH(3), HUMAN(4), ALIEN(5);
-    
+
     /**
      * Each type of sector is represented for convenience also by a number.
      */
     private int sectorNum;
-    
+
+    /**
+     * Correspondence between one sector and one of the numbers of the enumeration.
+     */
     private static Map<Integer, Sector> map = new HashMap<Integer, Sector>();
 
     static {
@@ -23,18 +26,21 @@ public enum Sector {
         }
     }
 
+    /**
+     * @param num The number of the type of sector.
+     */
     private Sector(final int num) { sectorNum = num; }
-    
+
     /**
      * @param the number of the sector type.
-     * @return 
+     * @return the sector number.
      */
     public static Sector valueOf(int sectorNum) {
         return map.get(sectorNum);
     }
-    
+
     /**
-     * @return the number of the sector type
+     * @return the number of the sector type.
      */
     public int getValue() {
         return sectorNum;
