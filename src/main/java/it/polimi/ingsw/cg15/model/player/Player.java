44a9590d0f0cc6ae0e0e6877630df2af487e9af6
Luke <Luke@192.168.2.123>
2015-06-15 11:31:34 +0200

    Fix Player.

--- src/main/java/it/polimi/ingsw/cg15/model/player/Player.java
+++ src/main/java/it/polimi/ingsw/cg15/model/player/Player.java
@@ -7,31 +7,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ *  * @author MMP - LMR
  * This is the instance of the model "Player", contains player information such as location, the list of cards that object and the type of player (human, alien). 
  * There is also the status that indicates whether the player is still alive or has been killed by an alien, in which case the variable status is false.
- * @author MMP - LMR
  */
 public class Player {
 
     /**
-     * Maximum number of objects that can be held simultaneously by a player is 3
+     * Maximum number of objects that can be held simultaneously by a player is 3.
      */
     public static final int MAX_ITEMCARD = 3;
-    
-    public static final int INGAME = 0;
-    public static final int WIN = 1;
-    public static final int KILLED = 2;
 
-    
+    /**
+     * Variable to determine whether or not a player is part of a game.
+     */
+    public static final int INGAME = 0;
+
+    /**
+     * Variable that determines whether a player has won.
+     */
+    public static final int WIN = 1;
+
+    /**
+     * Variable that determines whether a player has killed.
+     */
+    public static final int KILLED = 2;
 
     /**
      * The position of the player.
      */
     protected Cell position;
-    
-    
-    private int playerNumber;
 
+    /**
+     * The number of the player.
+     */
+    private int playerNumber;
 
     /**
      * List item cards the player owns. They are cards.
@@ -47,11 +57,11 @@ public class Player {
      * The status of a player indicates if it is still active (in the race) in the current game, or if he was killed and then deleted. Default is active.
      */
     private int status = INGAME;
-    
 
-    //TODO docs
+    /**
+     * Variable that determines if a player was able to reach an hatch sector and escape.
+     */
     private boolean escaped = false;
-
 
     /**
      * The constructor.
@@ -64,18 +74,17 @@ public class Player {
         cards = new ArrayList<ItemCard>(MAX_ITEMCARD);
     }
 
+    /**
+     * A constructor.
+     */
     public Player(){
-      cards = new ArrayList<ItemCard>(MAX_ITEMCARD);
-
-
-      
-
+        cards = new ArrayList<ItemCard>(MAX_ITEMCARD);
     }
 
     /**
      * This method returns the item cards held by the player via their identification.
-     * @param id
-     * @return
+     * @param id The identifier of the card.
+     * @return The card requested by a id.
      */
     public ItemCard getCardById(int id){
         return cards.get(id);
@@ -96,7 +105,6 @@ public class Player {
         return this.type;
     }
 
-
     /**
      * @return the current position of the player
      */
@@ -104,13 +112,12 @@ public class Player {
         return this.position;
     }
 
-
     /**
-     * @return a boolean that says if the player is still alive or not
+     * @return a boolean that says if the player is still alive or not.
      */
     public boolean isAlive() {
         if(status==INGAME){
-        return true;
+            return true;
         }
         else{
             return false;
@@ -118,7 +125,7 @@ public class Player {
     }
 
     /**
-     * @return says if the deck of cards the player's personal are present or not
+     * @return says if the deck of the player has cards or not.
      */
     public boolean isPersonalDeckEmpty(){
         return cards.isEmpty();
@@ -138,8 +145,8 @@ public class Player {
 
     /**
      * Removing object card in the player's possession.
-     * @param card
-     * @return
+     * @param card The card to eliminate.
+     * @return a boolean for the success of the operation.
      */
     public boolean removeCard(ItemCard card) {
         if(cards.remove(card)){
@@ -150,8 +157,8 @@ public class Player {
 
     /**
      * He adds the current player a card object available. The card must be passed to the function.
-     * @param card
-     * @return
+     * @param card The card to add.
+     * @return The card added.
      */
     public boolean addCard(ItemCard card){
         return cards.add(card);
@@ -159,50 +166,73 @@ public class Player {
 
     /**
      * This function takes as input a target cell and move there the current player.
-     * @param dest
+     * @param dest The destination.
      */
     public void setPosition(Cell dest) {
         this.position = dest;
     }
 
+    /**
+     * Set the type of the player.
+     * @param type The type to be set.
+     */
     public void setPlayerType(PlayerType type) {
         this.type=type;
     }
-    
-    
-    //TODO: docs
+
+    /**
+     * @return the number of the player.
+     */
     public int getPlayerNumber() {
         return playerNumber;
     }
 
+    /**
+     * @param playerNumber the player's number to be set.
+     */
     public void setPlayerNumber(int playerNumber) {
         this.playerNumber = playerNumber;
     }
 
-
+    /**
+     * @return The card list of a player.
+     */
     public List<ItemCard> getCardList() {
         return cards;
     }
-    
+
+    /**
+     * Method that checks whether a player has run away from the game. That is won.
+     * @return A boolean indicating whether or not the win.
+     */
     public boolean isEscaped() {
         if(status==WIN){
-        return true;
+            return true;
         }
         else{
             return false;
         }
     }
+
+    /**
+     * @return The status of the player.Win, Killed, InGame.
+     */
     public int getStatus() {
         return status;
     }
 
+    /**
+     * Method that sets a player escaped.
+     */
     public void setEscaped() {
-        
         setWin();
     }
 
- public void setWin() {
+    /**
+     * Method that puts the player in the state won.
+     */
+    public void setWin() {
         this.status = WIN;
     }
-    
+
 }
