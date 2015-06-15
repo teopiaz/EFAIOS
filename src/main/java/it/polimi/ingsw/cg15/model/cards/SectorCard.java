b25d5c7d2af162e460cd80c91b35a06ff02b943d
Luke <Luke@192.168.2.123>
2015-06-15 12:18:27 +0200

    Fix SectorCard.

--- src/main/java/it/polimi/ingsw/cg15/model/cards/SectorCard.java
+++ src/main/java/it/polimi/ingsw/cg15/model/cards/SectorCard.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.cg15.model.cards;
 
 /**
  * @author MMP - LMR
- * This are the hatch cards. They are of 5 types.
+ * This are the hatch cards. They are of 5 types. Silence, Red, RedItem, Green, GreenItem.
  */
 public enum SectorCard implements Card {
     SECTOR_SILENCE, SECTOR_RED, SECTOR_RED_ITEM, SECTOR_GREEN, SECTOR_GREEN_ITEM
