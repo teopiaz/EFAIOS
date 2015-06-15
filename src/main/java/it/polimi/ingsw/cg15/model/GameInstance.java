3eb06fef3a9e003c29d9dea05a4ed77d400bc2ae
Luke <Luke@192.168.2.123>
2015-06-15 14:37:15 +0200

    Fix GameInstance.

--- src/main/java/it/polimi/ingsw/cg15/model/GameInstance.java
+++ src/main/java/it/polimi/ingsw/cg15/model/GameInstance.java
@@ -6,19 +6,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 //TODO: singleton
+/**
+ * @author MMP - LMR
+ * The class that contains the varius game state. It allows to group information on the various games.
+ */
 public class GameInstance {
+    
+    /**
+     * The singleton instance of game instance.
+     */
     static GameInstance singletonInstance = new GameInstance();
+    
+    /**
+     * The list of instances of game state.
+     */
     private List<GameState> instanceList = new ArrayList<GameState>();
     
-
+    /**
+     * The constructor.
+     */
     private GameInstance() {
     }
+    
+    /**
+     * @return the current instance of game.
+     */
     public static GameInstance getInstance(){
         return singletonInstance;
     }
     
-    
-
+    /**
+     * Add a match into the instance of game.
+     * @return the game instance created.
+     */
     public GameState addGameInstance( ){
         Field field = new Field();
         DeckContainer deckContainer = new DeckContainer();
@@ -27,17 +47,20 @@ public class GameInstance {
         return gameState;
     }
     
-
-
+    /**
+     * @return the size of the instance list.
+     */
     public int getInstanceSize(){
         return instanceList.size();
 
     }
 
-    
-    
+    /**
+     * Remove a game state from the instance list.
+     * @param gameState The game state to remove.
+     * @return a boolean if tha game state has been removed.
+     */
     public boolean removeGameInstace(GameState gameState){
-        
         if(instanceList.remove(gameState)){
             return true;
         }
