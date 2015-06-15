c3506b7c866c42204f47728584602a01b25487d1
Luke <Luke@192.168.2.123>
2015-06-15 14:46:11 +0200

    Fix SuperalienPlayerController

--- src/main/java/it/polimi/ingsw/cg15/controller/player/SuperalienPlayerController.java
+++ src/main/java/it/polimi/ingsw/cg15/controller/player/SuperalienPlayerController.java
@@ -6,7 +6,7 @@ import it.polimi.ingsw.cg15.model.field.Coordinate;
 import it.polimi.ingsw.cg15.model.field.Field;
 
 /**
- * @author LMR - MMP
+ * @author MMP - LMR
  * The controller of the player.
  */
 public class SuperalienPlayerController extends PlayerController {
@@ -16,22 +16,25 @@ public class SuperalienPlayerController extends PlayerController {
      */
     private GameState gameState;
 
-
     /**
-     * @param gameState
+     * The constructor.
+     * @param gameState The current state of the game.
      */
     public SuperalienPlayerController(GameState gameState) {
         super(gameState);
         this.gameState = gameState;
     }
 
+    /**
+     * Check if a move is possible for the super alien type of player.
+     * @see it.polimi.ingsw.cg15.controller.player.PlayerController#moveIsPossible(it.polimi.ingsw.cg15.model.field.Coordinate)
+     */
     @Override
     public boolean moveIsPossible(Coordinate coord) {
         System.out.println("alien moveIsPossible");
         if(gameState.getTurnState().hasMoved()){
             return false;
         }
-       
         Field field = gameState.getField();
         if(!field.getField().containsKey(coord)){
             return false;
@@ -41,11 +44,14 @@ public class SuperalienPlayerController extends PlayerController {
         Cell destination = field.getCell(coord);
         return field.isReachable(currentPosition, destination, 3);
     }
+    
+    /**
+     * Check if I can use a item card.
+     * @see it.polimi.ingsw.cg15.controller.player.PlayerController#canUseCard()
+     */
     @Override
     public boolean canUseCard(){
         return false;
     }
     
-
-
 }
