0f3c624496f9bdb3f39dfefd5401f4114e43da6f
Luke <Luke@192.168.2.123>
2015-06-15 12:32:50 +0200

    Fix HatchCard.

--- src/main/java/it/polimi/ingsw/cg15/model/cards/HatchCard.java
+++ src/main/java/it/polimi/ingsw/cg15/model/cards/HatchCard.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.cg15.model.cards;
 
 /**
  * @author MMP - LMR
- * This are the hatch cards. They are of two types.
+ * This are the hatch cards. They are of two types. Green, free hatch sector. Red, broken hatch sector.
  */
 public enum HatchCard implements Card {
     HATCH_GREEN, HATCH_RED
