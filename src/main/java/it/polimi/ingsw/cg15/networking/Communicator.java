fcc3b0b43df6c81ab1af6f40f1d75bdfa0f0c58b
Luke <Luke@192.168.2.123>
2015-06-16 21:10:22 +0200

    Fix Communicator.

--- src/main/java/it/polimi/ingsw/cg15/networking/Communicator.java
+++ src/main/java/it/polimi/ingsw/cg15/networking/Communicator.java
@@ -1,11 +1,24 @@
 package it.polimi.ingsw.cg15.networking;
 
+/**
+ * @author MMP - LMR
+ * The interface for the communication.
+ */
 public interface Communicator {
 
+    /**
+     * @param msg The message.
+     */
     void send(String msg);
 
+    /**
+     * @return The string received.
+     */
     String receive();
 
+    /**
+     * Close the communication.
+     */
     void close();
 
 }
