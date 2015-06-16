9204bb36f1dfe148d90ab9c1a4ff484bdc560c76
Luke <Luke@192.168.2.123>
2015-06-16 16:30:31 +0200

    Fix MapLoader.

--- src/main/java/it/polimi/ingsw/cg15/utils/MapLoader.java
+++ src/main/java/it/polimi/ingsw/cg15/utils/MapLoader.java
@@ -17,8 +17,16 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * @author MMP - LMR
+ * The class that is responsible for loading and saving the map.
+ * Galilei, Galvani and Fermi are already available.
+ */
 public class MapLoader {
 
+    /**
+     * List of maps already created.
+     */
     private static final ArrayList<String> localMapList = new ArrayList<String>() ;
     
     static{
@@ -27,32 +35,32 @@ public class MapLoader {
         localMapList.add("fermi"); 
     }
 
-    
-    
+    /**
+     * The constructor.
+     */
     private MapLoader() {
-
     }
-
+    
+    /**
+     * Load a map.
+     * @param field The field.
+     * @param mapName The name of the map.
+     * @return true if the map has been created correctly.
+     */
     public static boolean loadMap(Field field, String mapName) {
         InputStream fin = null;
-
         if (mapName == null) {
             return false;
         }
-
         String fName ="maps/"+ mapName + ".txt";
-
         if(localMapList.contains(mapName)){
             fName =mapName + ".txt";
-
             fin = MapLoader.class.getClassLoader().getResourceAsStream(fName);
-
             if (fin == null) {
                 return false;
             }
         }
         else{
-            
             File file = new File(fName);
             try {
                 fin=new FileInputStream(file);
@@ -60,12 +68,9 @@ public class MapLoader {
                 Logger.getLogger(MapLoader.class.getName()).log(Level.SEVERE, "File "+ fName+" not found", e1);
                 return false;
             }
-            
         }
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(fin));
         String line = null;
-
         try {
             line = reader.readLine();
         } catch (IOException e1) {
@@ -77,8 +82,6 @@ public class MapLoader {
             int c = Integer.valueOf(splitted[1]);
             int r = Integer.valueOf(splitted[0]);
             int type = Integer.valueOf(splitted[2]);
-
-
             if (type!=0) {
                 Coordinate coord = new Coordinate(r, c);
                 field.addCell(coord, Sector.valueOf(type));
@@ -90,44 +93,36 @@ public class MapLoader {
                 }
                 if(type==5){
                     field.setAlienStartingPosition(field.getCell(coord));
-                }
-                
+                }   
             }
-            
-
             try {
                 line = reader.readLine();
             } catch (IOException e1) {
                 Logger.getLogger(MapLoader.class.getName()).log(Level.SEVERE, "MapLoad readline IOException", e1);
-
             }
         }
-
         try {
             reader.close();
         } catch (IOException e) {
             Logger.getLogger(MapLoader.class.getName()).log(Level.SEVERE, "MapLoad close IOException", e);
-
         }
-
         return true;
-
     }
 
+    /**
+     * Save a map.
+     * @param field The field.
+     * @param mapName The name of the map.
+     */
     public static void saveMap(Field field,String mapName) {
-
-
         FileOutputStream fop = null;
         File file;
-
         if(mapName==""){
             mapName="map";
         }
         try {
-
             file = new File("maps/"+mapName+".txt");
             fop = new FileOutputStream(file);
-
             // if file doesnt exists, then create it
             if (!file.exists()) {
                 file.createNewFile();
@@ -139,17 +134,13 @@ public class MapLoader {
                     Cell cell = field.getCell(new Coordinate(c, r));
                     if(cell!=null){
                         content=content+(r)+","+(c)+","+ cell.getSectorType().getValue() +"\n"; 
-
                     }
                 }
             }
             byte[] contentInBytes = content.getBytes();
-
             fop.write(contentInBytes);
             fop.flush();
             fop.close();
-
-
         } catch (IOException e) {
             Logger.getLogger(MapLoader.class.getName()).log(Level.SEVERE, "Mapsave IOException", e);
         } finally {
