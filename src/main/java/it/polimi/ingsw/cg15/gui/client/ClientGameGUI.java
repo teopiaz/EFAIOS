3fa86a87128de3dac1fc95404e7db1e025ff77f1
Luke <Luke@192.168.2.123>
2015-06-17 11:16:06 +0200

    Fix ClientGameGUI.

--- src/main/java/it/polimi/ingsw/cg15/gui/client/ClientGameGUI.java
+++ src/main/java/it/polimi/ingsw/cg15/gui/client/ClientGameGUI.java
@@ -28,58 +28,93 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+/**
+ * @author MMP - LMR
+ * The GUI of the client game.
+ */
 public class ClientGameGUI implements Runnable, ViewClientInterface {
 
+    /**
+     * The game board.
+     */
     int[][] board = new int[23][15];
+
+    /**
+     * The map panel.
+     */
     MapPanel map;
+
+    /**
+     * The frame.
+     */
     private JFrame frame;
+
+    /**
+     * The spotlight effect layer.
+     */
     SpotlightLayerUI spotlightLayerUI;
+
+    /**
+     * The panel for spotlight layer.
+     */
     JLayer<JPanel> jlayerSpot;
+
+    /**
+     * Enable or disable spotlight effect.
+     */
     boolean spotlightEnable = false;
+
+    /**
+     * A string with the map.
+     */
     String strmap;
+
+    /**
+     * The Network Helper.
+     */
     NetworkHelper netHelper;
 
+    /**
+     * Run the Client Game GUI.
+     */
     @Override
     public void run() {
         frame.setLocationRelativeTo(null);
         // frame.setVisible(true);
     }
 
+    /**
+     * The constructor.
+     * @param netHelper The network helper.
+     */
     public ClientGameGUI(NetworkHelper netHelper) {
         this.netHelper = netHelper;
         netHelper.registerGui(this);
         prepareFrame();
         System.out.println("GUI creato");
-
     }
 
+    /**
+     * Prepare the frame for the Client GUI.
+     */
     public void prepareFrame() {
-
         frame = new JFrame("Escape From Aliens in Outer Space");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-
         frame.setResizable(false);
-
         JPanel panel = new JPanel(true);
         panel.setBackground(Color.BLACK);
         panel.setLayout(new BorderLayout());
-
         map = new MapPanel(true, board);
         frame.setSize(1280, 720);
-
         JPanel sidepanel = new SidePanel();
-
         frame.add(sidepanel, BorderLayout.EAST);
         frame.add(map, BorderLayout.WEST);
         frame.add(panel);
-
         spotlightLayerUI = new SpotlightLayerUI();
         jlayerSpot = new JLayer<JPanel>(map, spotlightLayerUI);
         frame.add(jlayerSpot);
-
         JMenuBar menuBar = new JMenuBar();
-
         JMenu menu = new JMenu("File");
         JMenuItem saveMenu = new JMenuItem("Save");
         JMenuItem loadMenu = new JMenuItem("Load");
@@ -87,25 +122,37 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
         JMenuItem spotDebugMenu = new JMenuItem("Spotlight");
         JMenuItem editorDebugMenu = new JMenuItem("Editor Mode");
         JMenuItem loadmapDebugMenu = new JMenuItem("Load game map");
-
+        
         loadmapDebugMenu.addActionListener(new ActionListener() {
 
+            /**
+             * @param e The action event.
+             * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 loadMap();
             }
+
         });
 
         editorDebugMenu.addActionListener(new ActionListener() {
 
+            /**
+             * @param e The action event.
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 map.setEditorMode(!map.getEditorMode());
             }
+            
         });
 
         spotDebugMenu.addActionListener(new ActionListener() {
 
+            /**
+             * @param e The action event.
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (spotlightEnable == true) {
@@ -114,19 +161,20 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
                 } else {
                     spotlightLayerUI.disableSpot();
                     spotlightEnable = true;
-
                 }
-
             }
+            
         });
 
         loadMenu.addActionListener(new ActionListener() {
 
+            /**
+             * @param e The action event.
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 board = map.getBoard();
                 FileInputStream fin = null;
-
                 String mapName = JOptionPane.showInputDialog(frame, "Insert Map name to load");
                 if (mapName == "") {
                     mapName = "map";
@@ -136,36 +184,33 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
                     fin = new FileInputStream(file);
                 } catch (FileNotFoundException e1) {
                     Logger.getLogger(ClientGameGUI.class.getName()).log(Level.SEVERE, "File not found exception", e1);
-                    e1.printStackTrace();
                 }
                 BufferedReader reader = new BufferedReader(new InputStreamReader(fin));
-
                 String line = null;
-
                 try {
                     line = reader.readLine();
                 } catch (IOException e1) {
                     Logger.getLogger(ClientGameGUI.class.getName()).log(Level.SEVERE, "IO exception", e1);
-                    e1.printStackTrace();
                 }
                 while (line != null) {
                     String[] splitted = line.split(",");
-
                     board[Integer.valueOf(splitted[1]) - 1][Integer.valueOf(splitted[0]) - 1] = Integer.valueOf(splitted[2]);
-
                     try {
                         line = reader.readLine();
                     } catch (IOException e1) {
                         Logger.getLogger(ClientGameGUI.class.getName()).log(Level.SEVERE, "IO exception", e1);
-                        e1.printStackTrace();
                     }
                 }
                 map.repaint();
             }
-
+            
         });
 
         saveMenu.addActionListener(new ActionListener() {
+
+            /**
+             * @param event The action event.
+             */
             @Override
             public void actionPerformed(ActionEvent event) {
                 board = map.getBoard();
@@ -178,15 +223,12 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
                     mapName = "map";
                 }
                 try {
-
                     file = new File("maps/" + mapName + ".txt");
                     fop = new FileOutputStream(file);
-
                     // if file doesnt exists, then create it
                     if (!file.exists()) {
                         file.createNewFile();
                     }
-
                     // get the content in bytes
                     for (int i = 0; i < 15; i++) {
                         for (int j = 0; j < 23; j++) {
@@ -194,26 +236,25 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
                         }
                     }
                     byte[] contentInBytes = content.getBytes();
-
                     fop.write(contentInBytes);
                     fop.flush();
                     fop.close();
-
                     System.out.println("Done");
-
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    Logger.getLogger(ClientGameGUI.class.getName()).log(Level.SEVERE, "IO Exception", e);
                 } finally {
                     try {
                         if (fop != null) {
                             fop.close();
                         }
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        Logger.getLogger(ClientGameGUI.class.getName()).log(Level.SEVERE, "IO Exception", e);
                     }
                 }
             }
+            
         });
+        
         debugMenu.add(spotDebugMenu);
         debugMenu.add(editorDebugMenu);
         debugMenu.add(loadmapDebugMenu);
@@ -222,18 +263,22 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
         menuBar.add(menu);
         menuBar.add(debugMenu);
         frame.setJMenuBar(menuBar);
-
         frame.pack();
         // GraphicsDevice device =
         // GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
         // device.setFullScreenWindow(frame);
-
     }
 
+    /**
+     * Show the Client GUI.
+     */
     public void showGUI() {
         this.frame.setVisible(true);
     }
 
+    /**
+     * Load the map.
+     */
     public void loadMap() {
         board = new int[23][16];
         map.setBoard(board);
@@ -241,7 +286,6 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
         Scanner mapScanner = new Scanner(strmap);
         while (mapScanner.hasNextLine()) {
             String line = mapScanner.nextLine();
-
             String[] splitted = line.split(",");
             String strrr = splitted[0];
             String strc = splitted[1];
@@ -250,56 +294,60 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
             int c = Integer.parseInt(strc) - 1;
             int type = Integer.parseInt(strtype);
             board[c][r] = type;
-
         }
         mapScanner.close();
         map.repaint();
     }
 
+    /**
+     * @param msg The massage to print.
+     */
     @Override
     public void stampa(String msg) {
         SidePanel.getActionPanel().printMsg(msg);
     }
 
+    /**
+     * @param s The string to add as log.
+     */
     public void addToLog(String s) {
-
         SidePanel.getLogPanel().addToLog(s);
-
     }
 
+    /**
+     * Log an event e.
+     * @param e The event to log.
+     * @see it.polimi.ingsw.cg15.gui.ViewClientInterface#log(it.polimi.ingsw.cg15.networking.Event)
+     */
     @Override
     public void log(Event e) {
-        
         SidePanel.getActionPanel().getActionsList();
         SidePanel.getCardPanel().getCardsList();
-
-        
         if (e.getRetValues().containsKey("move")) {
             String player = e.getRetValues().get("player");
             String sector = e.getRetValues().get("move");
-            addToLog("Giocatore " + player + " si è mosso in " + sector);
+            addToLog("Player " + player + " has moved in: " + sector);
         }
         if (e.getRetValues().containsKey("attack")) {
             String playerNum = e.getRetValues().get("player");
             String position = e.getRetValues().get("attack");
-            addToLog("Giocatore " + playerNum + ": attacca nel settore " + position);
+            addToLog("Player " + playerNum + ": attacks in the sector: " + position);
             int count = 0;
             for (Entry<String, String> ret : e.getRetValues().entrySet()) {
                 if (ret.getValue().equals("killed")) {
-                    addToLog("Giocatore " + ret.getKey() + " ucciso dal giocatore " + playerNum);
+                    addToLog("Player " + ret.getKey() + " killed by the player: " + playerNum);
                     count++;
                 }
             }
             if (count == 0) {
-                addToLog("Nessuna Vittima");
+                addToLog("No player killed.");
             }
-
         }
         if (e.getRetValues().containsKey("noise")) {
             if (e.getRetValues().get("noise").equals("true")) {
                 String playerNum = e.getRetValues().get("player");
                 String position = e.getRetValues().get("position");
-                addToLog("Giocatore " + playerNum + ": rumore in settore " + position);
+                addToLog("Player " + playerNum + ": make noise in sector: " + position);
             }
         }
         if (e.getRetValues().containsKey("hatch")) {
@@ -307,33 +355,42 @@ public class ClientGameGUI implements Runnable, ViewClientInterface {
                 addToLog(e.getRetValues().get("message"));
             } else {
                 String player = e.getRetValues().get("player");
-                addToLog("il giocatore" + player + " ha pescato una hatch card "
+                addToLog("Player" + player + " has drawn a hatch card: "
                         + e.getRetValues().get("hatchcard"));
             }
         }
     }
 
+    /**
+     * @param e The event to add as chat.
+     * @see it.polimi.ingsw.cg15.gui.ViewClientInterface#chat(it.polimi.ingsw.cg15.networking.Event)
+     */
     public void chat(Event e) {
-        String message = "[Giocatore " + e.getRetValues().get("player") + "]" + " "+ e.getRetValues().get("message");
+        String message = "[Player " + e.getRetValues().get("player") + "]" + " "+ e.getRetValues().get("message");
         SidePanel.getChatPanel().addToChat(message);
-
     }
 
+    /**
+     * Set the game as started.
+     * @see it.polimi.ingsw.cg15.gui.ViewClientInterface#setStarted()
+     */
     public void setStarted() {
         loadMap();
-        addToLog("La Partita è cominciata!");
+        addToLog("The game started now!");
         SidePanel.getActionPanel().getActionsList();
     }
 
+    /**
+     * The current player turn.
+     * @see it.polimi.ingsw.cg15.gui.ViewClientInterface#currentPlayer(int)
+     * TODO scrivere le println in inglese!
+     */
     @Override
     public void currentPlayer(int currentPlayer) {
-    	System.out.println("È il turno del giocatore "+ currentPlayer);
+        System.out.println("È il turno del giocatore "+ currentPlayer);
         addToLog("È il turno del giocatore "+ currentPlayer);
         SidePanel.getActionPanel().getActionsList();
         SidePanel.getCardPanel().getCardsList();
-
-        
     }
-    
 
 }
