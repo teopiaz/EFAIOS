fa9e7b514ed69eacd38278f407f07921c1613fc6
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2015-06-23 16:23:14 +0200

    Doc TimerTurn.

--- src/main/java/it/polimi/ingsw/cg15/utils/TimerTurn.java
+++ src/main/java/it/polimi/ingsw/cg15/utils/TimerTurn.java
@@ -4,20 +4,41 @@ import it.polimi.ingsw.cg15.controller.GameController;
 
 import java.util.TimerTask;
 
+/**
+ * @author MMP - LMR
+ * The class for timer of the turn.
+ */
 public class TimerTurn extends TimerTask {
     
+    /**
+     * Interrupted state.
+     */
     private boolean interrupted=false;
     
+    /**
+     * The game controller.
+     */
     private GameController gc;
     
+    /**
+     * Timer turn.
+     * @param gc The game controller.
+     */
     public TimerTurn(GameController gc){
         this.gc = gc;
     }
     
+    /**
+     * Interrupted timer is true.
+     */
     public void interruptTimer(){
         this.interrupted=true;
     }
     
+    /**
+     * Run the timer.
+     * @see java.util.TimerTask#run()
+     */
     @Override
     public void run() {
         if(!interrupted)
